da8e8d1edf0f6a42a73333f2f7a8b6bda8562bb4
EvrardKodia <kodia.evrard@gmail.com>
2025-07-28 23:23:33 +0000

    Fix: ajout de prisma generate dans render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Mise à jour et installation de timidity
 apt-get update
 apt-get install -y timidity
+
+# Installation des dépendances Python
 pip install -r requirements.txt
+
+# Installation des dépendances Node.js
 npm install
+
+# Génération du client Prisma
+npx prisma generate
